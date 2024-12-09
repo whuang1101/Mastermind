6d555e27fe82ad1309be259fb970b1ce350ca726
Wilson Huang <wilsonhuang11012000@gmail.com>
2024-12-09 14:28:17 -0500

    Player order is maintained and player history is stored

--- game_logic/__init__.py
+++
@@ -1,2 +0,0 @@
-from .core import Game
-from .utils import get_random_numbers
--- main.py
+++
@@ -1,6 +0,0 @@
-import tkinter as tk
-from ui.app import MasterMindApp
-if __name__ == "__main__":
-    app = MasterMindApp()
-
-    app.mainloop()
