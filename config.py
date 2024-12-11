68b7db2ed30ea2d2b2309969d41d101ffda0754c
Wilson Huang <wilsonhuang11012000@gmail.com>
2024-12-10 19:21:58 -0500

    Added leaderboard and requirments.txt for easier download of necessary libraries

--- config.py
+++
@@ -1,3 +0,0 @@
-CACHE_TIMEOUT = 300
-DATABASE_PATH = 'database/game_data.db'
-DEBUG = True
--- routes/utils.py
+++
@@ -1,5 +0,0 @@
-import bcrypt
-def hash_password(password):
-    encrypted_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode('utf-8')
-    return encrypted_password
-
